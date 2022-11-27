781a816d5005bca476d212bd2b336c6718a989a0
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-11-27 15:13:00 +0530

    Swap of two number

--- BasicPrograms/BasicPrograms/Powerof2.cs
+++ BasicPrograms/BasicPrograms/Powerof2.cs
@@ -8,5 +8,20 @@ namespace BasicPrograms
 {
     internal class Powerof2
     {
+        public void Power_Of_2()
+        {
+            Console.WriteLine("Enter a number : ");
+            string inputByUser = Console.ReadLine();
+            int numVal = Int32.Parse(inputByUser);
+            if (numVal >= 0 || numVal < 31)
+            {
+                int temp = 1;
+                for (int i = 0; i < numVal; i++)
+                {
+                    temp = 2 * temp;
+                    Console.WriteLine(temp);
+                }
+            }
+        }
     }
 }
---
+++ BasicPrograms/BasicPrograms/SwapNo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class SwapNo
+    {
+        public void Swap_Two_Number()
+        {
+            int num1 = 12;
+            int num2 = 23;
+            int temp;
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.WriteLine("Swapping of number is  : " + num1 + " " + num2);
+        }
+    }
+}
