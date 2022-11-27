354780ac8dcde1f1f835a5c6e2928e100f34ffe7
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-11-27 14:42:02 +0530

    check whether the alphabet either the vowels or consonants

---
+++ BasicPrograms/BasicPrograms/Alphabet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class Alphabet
+    {
+        public void Check_Char_Vowel_Or_Consonant()
+        {
+            char ch;
+
+            Console.WriteLine("Enter any character: ");
+            ch = Convert.ToChar(Console.ReadLine());
+
+
+            // Condition for vowel checking
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+
+                Console.WriteLine(ch + " is Vowel.");
+
+            }
+            else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+            {
+                Console.WriteLine(ch + " is Consonant.");
+            }
+        }
+    }
+}
---
+++ BasicPrograms/BasicPrograms/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BasicPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Alphabet num = new Alphabet();
+            num.Check_Char_Vowel_Or_Consonant();
+        }
+    }
+}
