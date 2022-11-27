4739f072104d8154e1af39d7ffa7ecaa276aa958
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-11-27 15:10:08 +0530

    Power of 2

--- BasicPrograms/BasicPrograms/leapyear.cs
+++ BasicPrograms/BasicPrograms/leapyear.cs
@@ -8,5 +8,24 @@ namespace BasicPrograms
 {
     internal class leapyear
     {
+        public void Leap_Year()
+        {
+            int year = 2022;
+            if (year > 1000 && year < 9999)
+            {
+                if (year % 4 == 0 || year % 400 == 0)
+                {
+                    Console.WriteLine("Leay year.");
+                }
+                else
+                {
+                    Console.WriteLine("not leap year");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong Input .");
+            }
+        }
     }
 }
